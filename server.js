172f2712ecad83f90615acda03f401c1b9a9705b
AbdulRehman1357 <syedabdulrehmansaadat@gmail.com>
2026-05-13 01:44:13 +0500

    fix: moved css and js into public folder for styling

--- server.js
+++ server.js
@@ -16,16 +16,16 @@ const ADMIN_PASSWORD = 'admin123';
 const SECRET_KEY = 'your-secret-key-change-this-in-production';
 
 
-// const db = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   port: process.env.DB_PORT,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 const path = require('path');
 
 
